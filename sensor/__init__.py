b98d0eef48a3338814254e235dabd39490e91fa5
Liang Mingjing <liangmingjing849@163.com>
2023-08-18 15:21:17 +0800

    added more sensors
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

--- sensor/__init__.py
+++ sensor/__init__.py
@@ -2,3 +2,5 @@ from .camera_manager import *
 from .collision_sensor import *
 from .gnss_sensor import *
 from .lane_invasion_sensor import *
+from .imu_sensor import *
+from .radar_sensor import *
