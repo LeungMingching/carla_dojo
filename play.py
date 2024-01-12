3e233f974775dc2be5fb12f187122b48a72206de
Liang Mingjing <liangmingjing849@163.com>
2024-01-12 15:37:44 +0800

    manage config with hydra
    Signed-off-by: Liang Mingjing <liangmingjing849@163.com>

--- play.py
+++ play.py
@@ -1,6 +1,6 @@
 """Example of automatic vehicle control from client side."""
 
-import argparse
+import hydra
 import logging
 import pygame
 import carla
@@ -16,7 +16,7 @@ from agent import (
 from utils import KeyboardControl
 
 
-def game_loop(args):
+def game_loop(config):
     """
     Main loop of the simulation. It handles updating all the HUD information,
     ticking the agent and, if needed, the world.
@@ -27,16 +27,16 @@ def game_loop(args):
     world = None
 
     try:
-        if args.seed:
-            random.seed(args.seed)
+        if config['seed']:
+            random.seed(config['seed'])
 
-        client = carla.Client(args.host, args.port)
+        client = carla.Client(config['host'], config['port'])
         client.set_timeout(60.0)
 
         traffic_manager = client.get_trafficmanager()
         sim_world = client.get_world()
 
-        if args.sync:
+        if config['sync']:
             settings = sim_world.get_settings()
             settings.synchronous_mode = True
             settings.fixed_delta_seconds = 0.05
@@ -45,23 +45,23 @@ def game_loop(args):
             traffic_manager.set_synchronous_mode(True)
 
         display = pygame.display.set_mode(
-            (args.width, args.height),
+            (config['resolution']['width'], config['resolution']['height']),
             pygame.HWSURFACE | pygame.DOUBLEBUF)
 
-        hud = HUD(args.width, args.height)
-        world = World(client.get_world(), hud, args)
+        hud = HUD(config['resolution']['width'], config['resolution']['height'])
+        world = World(client.get_world(), hud, config)
         controller = KeyboardControl(world)
-        if args.agent == "Basic":
+        if config['agent'] == "Basic":
             agent = BasicAgent(world.player, 30)
             agent.follow_speed_limits(True)
-        elif args.agent == "Constant":
+        elif config['agent'] == "Constant":
             agent = ConstantVelocityAgent(world.player, 30)
             ground_loc = world.world.ground_projection(world.player.get_location(), 5)
             if ground_loc:
                 world.player.set_location(ground_loc.location + carla.Location(z=0.01))
             agent.follow_speed_limits(True)
-        elif args.agent == "Behavior":
-            agent = BehaviorAgent(world.player, behavior=args.behavior)
+        elif config['agent'] == "Behavior":
+            agent = BehaviorAgent(world.player, behavior=config['behavior'])
 
         # Set the agent destination
         spawn_points = world.map.get_spawn_points()
@@ -72,7 +72,7 @@ def game_loop(args):
 
         while True:
             clock.tick()
-            if args.sync:
+            if config['sync']:
                 world.world.tick()
             else:
                 world.world.wait_for_tick()
@@ -84,7 +84,7 @@ def game_loop(args):
             pygame.display.flip()
 
             if agent.done():
-                if args.loop:
+                if config['loop']:
                     agent.set_destination(random.choice(spawn_points).location)
                     world.hud.notification("Target reached", seconds=4.0)
                     print("The target has been reached, searching for another target")
@@ -109,80 +109,19 @@ def game_loop(args):
 
         pygame.quit()
 
-def main():
+@hydra.main(config_path="config/config.yaml")
+def main(config):
     """Main method"""
 
-    argparser = argparse.ArgumentParser(
-        description='CARLA Reinforcement Learning Client')
-    argparser.add_argument(
-        '-v', '--verbose',
-        action='store_true',
-        dest='debug',
-        help='Print debug information')
-    argparser.add_argument(
-        '--host',
-        metavar='H',
-        default='127.0.0.1',
-        help='IP of the host server (default: 127.0.0.1)')
-    argparser.add_argument(
-        '-p', '--port',
-        metavar='P',
-        default=2000,
-        type=int,
-        help='TCP port to listen to (default: 2000)')
-    argparser.add_argument(
-        '--res',
-        metavar='WIDTHxHEIGHT',
-        default='1280x720',
-        help='Window resolution (default: 1280x720)')
-    argparser.add_argument(
-        '--sync',
-        action='store_true',
-        help='Synchronous mode execution')
-    argparser.add_argument(
-        '--filter',
-        metavar='PATTERN',
-        default='vehicle.*',
-        help='Actor filter (default: "vehicle.*")')
-    argparser.add_argument(
-        '--generation',
-        metavar='G',
-        default='2',
-        help='restrict to certain actor generation (values: "1","2","All" - default: "2")')
-    argparser.add_argument(
-        '-l', '--loop',
-        action='store_true',
-        dest='loop',
-        help='Sets a new random destination upon reaching the previous one (default: False)')
-    argparser.add_argument(
-        "-a", "--agent", type=str,
-        choices=["Behavior", "Basic", "Constant"],
-        help="select which agent to run",
-        default="Behavior")
-    argparser.add_argument(
-        '-b', '--behavior', type=str,
-        choices=["cautious", "normal", "aggressive"],
-        help='Choose one of the possible agent behaviors (default: normal) ',
-        default='normal')
-    argparser.add_argument(
-        '-s', '--seed',
-        help='Set seed for repeating executions (default: None)',
-        default=None,
-        type=int)
-
-    args = argparser.parse_args()
-
-    args.width, args.height = [int(x) for x in args.res.split('x')]
-
-    log_level = logging.DEBUG if args.debug else logging.INFO
+    log_level = logging.DEBUG if config['verbose'] else logging.INFO
     logging.basicConfig(format='%(levelname)s: %(message)s', level=log_level)
 
-    logging.info('listening to server %s:%s', args.host, args.port)
+    logging.info('listening to server %s:%s', config['host'], config['port'])
 
     print(__doc__)
 
     try:
-        game_loop(args)
+        game_loop(config)
 
     except KeyboardInterrupt:
         print('\nCancelled by user. Bye!')
